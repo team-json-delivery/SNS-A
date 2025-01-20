3f9f9d7827dbc49ca86e2c98046e61ba4df607a1
Junho <chojunho433@gmail.com>
2025-01-20 22:12:17 +0900

    value class로 변경

--- src/main/kotlin/json/delivery/socialnetworkservice/app/domain/UserId.kt
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/domain/UserId.kt
@@ -1,6 +1,7 @@
 package json.delivery.socialnetworkservice.app.domain
 
-data class UserId(val id: Long) {
+@JvmInline
+value class UserId(val id: Long) {
     init {
         require(id > 0) { "UserId must be a positive number, but was $id" }
     }
