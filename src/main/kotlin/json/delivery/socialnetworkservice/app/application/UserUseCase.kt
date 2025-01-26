2d17979a78d003cfdc6b4f977114288992ec1752
Junho <chojunho433@gmail.com>
2025-01-26 16:10:41 +0900

    RelationUseCase 분리

--- src/main/kotlin/json/delivery/socialnetworkservice/app/application/UserUseCase.kt
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/application/UserUseCase.kt
@@ -7,9 +7,4 @@ import json.delivery.socialnetworkservice.app.domain.UserId
 interface UserUseCase {
     suspend fun getUserByUserId(userId: Long): User
     suspend fun registerUser(name: String): User
-
-    // TODO check
-    suspend fun followUser(userId: UserId, followerId: UserId): Relation
-    suspend fun unFollowUser(userId: UserId, followerId: UserId): Relation
-    suspend fun follower(userId: UserId): Relation
 }
