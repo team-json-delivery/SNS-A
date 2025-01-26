6dd4971d3ca8be1d19940fa7456a721c37cd3dfd
Junho <chojunho433@gmail.com>
2025-01-26 16:07:37 +0900

    follow 관련 함수 파라미터 타입을 UserId로 변경

--- src/main/kotlin/json/delivery/socialnetworkservice/app/application/UserUseCase.kt
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/application/UserUseCase.kt
@@ -1,15 +1,15 @@
 package json.delivery.socialnetworkservice.app.application
 
-import json.delivery.socialnetworkservice.app.domain.Following
 import json.delivery.socialnetworkservice.app.domain.Relation
 import json.delivery.socialnetworkservice.app.domain.User
+import json.delivery.socialnetworkservice.app.domain.UserId
 
 interface UserUseCase {
     suspend fun getUserByUserId(userId: Long): User
     suspend fun registerUser(name: String): User
 
     // TODO check
-    suspend fun followUser(userId: Long, followerId: Following): Relation
-    suspend fun unFollowUser(userId: Long, followerId: Following): Relation
-    suspend fun follower(userId: Long): Relation
+    suspend fun followUser(userId: UserId, followerId: UserId): Relation
+    suspend fun unFollowUser(userId: UserId, followerId: UserId): Relation
+    suspend fun follower(userId: UserId): Relation
 }
