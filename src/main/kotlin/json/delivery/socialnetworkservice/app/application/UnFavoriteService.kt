04657bcc7890578b0a57b2f72f59b66256c48fac
zoster0828 <zoster0828@gmail.com>
2025-02-02 16:29:52 +0900

    UnFavorite 동작을 처리할 수 있는 usecase 추가

---
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/application/UnFavoriteService.kt
@@ -0,0 +1,22 @@
+package json.delivery.socialnetworkservice.app.application
+
+import json.delivery.socialnetworkservice.app.application.dto.FavoriteUsecaseOutputDto
+import json.delivery.socialnetworkservice.app.application.dto.UnFavoriteUsecaseOutputDto
+import json.delivery.socialnetworkservice.app.domain.UserId
+import json.delivery.socialnetworkservice.app.infrastructure.ArticleRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UnFavoriteService(
+    private val articleRepository : ArticleRepository
+) : UnFavoriteUsecase {
+    override fun execute(userId: UserId, articleId: String): UnFavoriteUsecaseOutputDto {
+        val article = articleRepository.findById(articleId)
+
+        article.disLike(userId)
+
+        articleRepository.save(article)
+
+        return UnFavoriteUsecaseOutputDto(userId, articleId)
+    }
+}
