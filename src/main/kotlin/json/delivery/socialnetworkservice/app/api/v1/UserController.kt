a045602fe54e41e6820d43a3a95a2377ddc98ffc
junho3 <chojunho433@gmail.com>
2025-02-02 13:25:49 +0900

    컨트롤러 구현

--- src/main/kotlin/json/delivery/socialnetworkservice/app/api/v1/UserController.kt
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/api/v1/UserController.kt
@@ -1,6 +1,7 @@
 package json.delivery.socialnetworkservice.app.api.v1
 
 import json.delivery.socialnetworkservice.app.api.v1.request.FollowRequest
+import json.delivery.socialnetworkservice.app.api.v1.request.UnFollowRequest
 import json.delivery.socialnetworkservice.app.api.v1.response.FollowResponse
 import json.delivery.socialnetworkservice.app.application.RelationUseCase
 import json.delivery.socialnetworkservice.app.domain.UserId
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -25,6 +27,15 @@ class UserController(
         @Validated @RequestBody request: FollowRequest,
     ): FollowResponse {
         return relationUseCase.followUser(UserId(userId), UserId(request.followerId))
+            .let { FollowResponse(it) }
+    }
+
+    @PutMapping("/{userId}/unFollow")
+    fun unFollowUser(
+        @PathVariable userId: Long,
+        @Validated @RequestBody request: UnFollowRequest,
+    ): FollowResponse {
+        return relationUseCase.unFollowUser(UserId(userId), UserId(request.unFollowerId))
             .let { FollowResponse(it) }
     }
 }
