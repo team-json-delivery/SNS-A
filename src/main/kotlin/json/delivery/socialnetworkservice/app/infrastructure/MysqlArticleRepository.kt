7d6db37dc72aab71d8ca7eae4c015c211e690c70
zoster0828 <zoster0828@gmail.com>
2025-02-07 00:29:30 +0900

    MysqlArticleRepository implementation

---
+++ src/main/kotlin/json/delivery/socialnetworkservice/app/infrastructure/MysqlArticleRepository.kt
@@ -0,0 +1,39 @@
+package json.delivery.socialnetworkservice.app.infrastructure
+
+import json.delivery.socialnetworkservice.app.domain.Article
+import json.delivery.socialnetworkservice.app.entity.ArticleLikeDto
+import json.delivery.socialnetworkservice.app.exception.ArticleNotFoundException
+import json.delivery.socialnetworkservice.app.mapper.ArticleDtoFactory
+import org.springframework.stereotype.Repository
+
+@Repository
+class MysqlArticleRepository(
+    private val articleJpaClient: ArticleJpaClient,
+    private val articleLikeJpaClient: ArticleLikeJpaClient
+) : ArticleRepository {
+    override fun findById(id: String): Article {
+        val articleEntity =
+            articleJpaClient.findById(id).orElseThrow { ArticleNotFoundException("Article not found : $id") }
+        val likes = articleLikeJpaClient.findByArticleId(id).map { it.userId }
+        return Article.of(
+            id = articleEntity.id,
+            authorId = articleEntity.authorId,
+            content = articleEntity.content,
+            createdAt = articleEntity.createdAt,
+            updatedAt = articleEntity.updatedAt,
+            likes = likes
+        )
+    }
+
+    override fun save(article: Article): Article {
+        val articleEntity = ArticleDtoFactory.toEntity(article)
+        articleJpaClient.save(articleEntity)
+
+        article.likes.forEach { userId ->
+            val likeEntity = ArticleLikeDto(articleId = article.id, userId = userId.id)
+            articleLikeJpaClient.save(likeEntity)
+        }
+
+        return article
+    }
+}
